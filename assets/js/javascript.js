e422738689242e9205092ba229db512269572699
DavidDock <ddocksey@live.co.uk>
2023-03-31 13:06:48 +0000

    fix moving background by using css fixed

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -12,22 +12,26 @@ const meadow = document.getElementById("meadow");
 const body = document.getElementsByTagName("body")[0]
 const gameContainer = document.getElementById("game-container");
 const yourImage = document.getElementById("your-image");
+const computerImage = document.getElementById("computer-image")
 const animals = document.getElementsByClassName("animals");
 
 //all let variables needed through game loops
 let userChoice
+let computerChoice
 
 //get each animal choice from array
 //add event listener for each animal
 //determine user choice from click
 //display choice as image in html
+//run functions getComputerChoice
 for (let animal of animals) {
   animal.addEventListener("click", function() {
     userChoice = this.getAttribute("id");
     yourImage.src = `assets/images/${userChoice}.png`;
     yourImage.alt = `image of your animal choice, which was ${userChoice}`;
+    getComputerChoice();
   })
-}
+  }
 
 //event listeners for correct and incorrect answer to question
 //corect answer (yes button) takes user to rules area
@@ -38,6 +42,16 @@ yes.addEventListener('click', function() {
 });
 
 no.addEventListener('click', incorrect);
+
+//function inncorect to inform user answer is wrong
+function incorrect() {
+  questionSection.style.backgroundColor= "#fd0e35";
+  wrong.style.display= "block";
+  answer.style.display= "none";
+  no.style.display= "none";
+  question.style.display= "none";
+  yes.innerHTML= "Play Game"; 
+}
 
 //event listeners for destination choice buttons
 //runs functions inside event listner to move to relevant game area
@@ -67,3 +81,22 @@ function incorrect() {
     question.style.display= "none";
     yes.innerHTML= "Play Game"; 
   }
+
+  //function to pick random number for computer
+  //asign the computer a choice
+  //display comuter choice image in html
+  //function inncorect to inform user answer is wrong
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+     computerChoice = 'elephant';
+  }
+  if (randomNumber === 1) {
+     computerChoice = 'mouse';
+  }
+  if (randomNumber === 2) {
+     computerChoice = 'monkey';
+  }
+  computerImage.src = `assets/images/${computerChoice}.png`;
+  computerImage.alt = `image of the animal the computer choice, which was ${computerChoice}`;
+}
